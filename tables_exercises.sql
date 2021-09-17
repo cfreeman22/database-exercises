16e01b980f3cfa4ca6339a52dc17767e87eaf31b
Christian Freeman <freemanchristian410@gmail.com>
2021-09-17 14:48:39 -0500

    No Changes made

--- tables_exercises.sql
+++ tables_exercises.sql
@@ -14,7 +14,7 @@ select * from departments;
 select * from employees;
 -- The department number contains a numeric type, the title table contains a string and the date type in the from_date  to to_date 
 
--- The relationship between employees and departments is that we may identify which employee is a staff of a particular department. bb
+-- The relationship between employees and departments is that we may identify which employee is a staff of a particular department.
 -- 10)
 show create table dept_manager;
 -- CREATE TABLE `dept_manager` (↵  `emp_no` int NOT NULL,↵  `dept_no` char(4) NOT NULL,↵  `from_date` date NOT NULL,↵  `to_date` date NOT NULL,↵  PRIMARY KEY (`emp_no`,`dept_no`),↵  KEY `dept_no` (`dept_no`),↵  CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE ON UPDATE RESTRICT,↵  CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE ON UPDATE RESTRICT↵) ENGINE=InnoDB DEFAULT CHARSET=latin1
