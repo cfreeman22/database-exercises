299b19d3bc1a6b3148553d511ad6fc3ab9676265
Christian Freeman <freemanchristian410@gmail.com>
2021-09-23 08:54:33 -0500

    Using dezcribe and show command

---
+++ describe_and_show_command.sql
@@ -0,0 +1,19 @@
+-- SQL exercise Describe and show table
+-- Describe command
+DESCRIBE current_dept_emp;
+DESCRIBE departments;
+DESCRIBE dept_emp;
+DESCRIBE dept_emp_latest_date;
+DESCRIBE dept_manager;
+DESCRIBE employees;
+DESCRIBE salaries;
+DESCRIBE titles;
+-- SHOW CREATE table 
+SHOW CREATE TABLE titles;
+SHOW CREATE TABLE salaries;
+SHOW CREATE TABLE employees;
+SHOW CREATE TABLE dept_manager;
+SHOW CREATE TABLE dept_emp_latest_date;
+SHOW CREATE TABLE dept_emp;
+SHOW CREATE TABLE departments;
+SHOW CREATE TABLE current_dept_emp;
